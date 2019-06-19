c774135300c57d71b86c90aeeffdb487b976739e
taekimlux <kim@taehee.lu>
2019-06-20 00:35:54 +0200

    meme-generator :D

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+    return(
+        <header>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgKCYQwYN-XtiNy-yE8vgrshaM1_OpbbLN2TSZMwvRQ9ewyeK8" alt="doge-pic"/>
+            <p>Generate Your Meme</p>
+        </header>
+    )
+}
+
+export default Header
