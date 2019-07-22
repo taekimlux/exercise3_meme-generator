1a2871d66290d6ebc9196371dfcd6bc032f7a52e
taekimlux <kim@taehee.lu>
2019-07-22 21:19:36 +0200

    Updates

---
+++ precache-manifest.c3e747bc89431267ac7aaa02f2772752.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b9d98330db0ad0b4a1420a976f79b0",
+    "url": "/exercise3_meme-generator/index.html"
+  },
+  {
+    "revision": "0f19e2518a3a17b37f23",
+    "url": "/exercise3_meme-generator/static/js/2.e2adb5f9.chunk.js"
+  },
+  {
+    "revision": "62f762b61a11bdd9722b",
+    "url": "/exercise3_meme-generator/static/js/main.52069b56.chunk.js"
+  },
+  {
+    "revision": "04395c132b77a1558b6c",
+    "url": "/exercise3_meme-generator/static/js/runtime~main.2399d327.js"
+  }
+]);
